f813b959bc0e04eb6a42cdc4d5dbcff0366eac5c
Arash <72198577+ArashKarampour@users.noreply.github.com>
2025-08-18 13:37:44 +0000

    feat: adding the authorization for a method in home views.py

--- home/views.py
+++ home/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
+from django.contrib.auth.decorators import login_required
 
 # Create your views here:
 
@@ -9,6 +10,10 @@ from datetime import datetime
 
 def home(request):
     return render(request, 'home/welcome.html', {'today': datetime.today()})
+
+@login_required(login_url='/admin')
+def authorized(request):
+    return render(request, 'home/authorized.html', {})
 
 def home2(request):
     return HttpResponse("This is the app2")
