ef560f2d94900c0368f81c6e0e7d9909d0a686ba
Arash <72198577+ArashKarampour@users.noreply.github.com>
2025-08-25 17:19:01 +0000

    feat: adding the 2 class based views in home views.py and the corresponfing urls.py

--- home/urls.py
+++ home/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('home', views.home),
-    path('authorized', views.authorized),
+    path('home', views.HomeView.as_view()),  # Class Based View for home
+    # path('home', views.home),
+    path('authorized', views.AuthorizedView.as_view()),
+    # path('authorized', views.authorized),
 ]
--- home/views.py
+++ home/views.py
@@ -2,18 +2,29 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import datetime
 from django.contrib.auth.decorators import login_required
+from django.views.generic import TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here:
 
 # def home(request):
 #     return HttpResponse("Hello World!")
 
-def home(request):
-    return render(request, 'home/welcome.html', {'today': datetime.today()})
+# def home(request):
+#     return render(request, 'home/welcome.html', {'today': datetime.today()})
 
-@login_required(login_url='/admin')
-def authorized(request):
-    return render(request, 'home/authorized.html', {})
+# Class Based View for home method(view):
+class HomeView(TemplateView):
+    template_name = 'home/welcome.html'
+    extra_context = {'today': datetime.today()}
+
+# @login_required(login_url='/admin')
+# def authorized(request):
+#     return render(request, 'home/authorized.html', {})
+
+class AuthorizedView(LoginRequiredMixin, TemplateView):
+    login_url = '/admin'
+    template_name = 'home/authorized.html'
 
 def home2(request):
     return HttpResponse("This is the app2")
